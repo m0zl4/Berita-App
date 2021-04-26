a41f599c980470b7b91405e4ad8763a9a7bb935d
m0zl4 <zayuka040104@gmail.com>
2021-04-26 10:00:26 +0700

    Finished

---
+++ NewsAPI/View/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  NewsAPI
+//
+//  Created by M. Abdul Latief -Mz on 15/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    TabView{
+      NewsList()
+        .tabItem{
+          Image(systemName: "house.fill")
+          Text("News")
+        }
+      
+      SearchList()
+        .tabItem{
+          Image(systemName: "magnifyingglass")
+          Text("Search")
+        }
+      
+      FavoriteNewsList()
+        .tabItem{
+          Image(systemName: "heart")
+          Text("Favorite")
+        }
+      
+      Profile()
+        .tabItem{
+          Image(systemName: "person.fill")
+          Text("About")
+        }
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
